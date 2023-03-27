07d787e43dbe30de035baf2de53a43817026ea65
caio lucena <caio.lcolaco@gmail.com>
2023-03-27 20:07:53 -0300

    componentizing the pokedex

---
+++ components/PokedexLeft.tsx
@@ -0,0 +1,8 @@
+export default function PokedexLeft(props: any) {
+
+    return (
+        <>
+            <img src="/PokePart1.png" alt="Parte 1 do pokedex" />
+        </>
+    )
+}
---
+++ components/PokedexRight.tsx
@@ -0,0 +1,8 @@
+export default function PokedexRight(props: any) {
+
+    return (
+        <>
+            <img src="/PokePart23.png" alt="Parte 2 do pokedex" />
+        </>
+    )
+}
