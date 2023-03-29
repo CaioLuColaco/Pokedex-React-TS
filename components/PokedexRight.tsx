383e939c05309a5f8abfc79ad6075f9a2348342a
caio lucena <caio.lcolaco@gmail.com>
2023-03-29 15:39:56 -0300

    added buttons

--- components/PokedexRight.tsx
+++ components/PokedexRight.tsx
@@ -1,10 +1,20 @@
+import pokeApi from "../services/pokeApi"
 import styles from "@/styles/PokedexRight.module.css"
+import { useEffect, useState } from "react"
 
 export default function PokedexRight(props: any) {
+    // const pokemons = useState([])
+    // useEffect(() => {
+
+    // },[])
+
+
 
     return (
         <div className={styles.rightSide}>
             <img src="/pokedexRight.png" alt="Parte 2 do pokedex" className={styles.imgPokedex}/>
+
+            <img src="/visorRight.png" alt="visor pokemons" className={styles.imgVisor}/>
 
             <div className={styles.contentBox}>
                 <div className={styles.searchBox}>
@@ -13,10 +23,18 @@ export default function PokedexRight(props: any) {
                 </div>
 
                 <div className={styles.pokemonsBox}>
-                    <div className={styles.cards}>
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/649.gif" alt="" />
-                        
+                    <div className={styles.card}>
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/30.gif" alt="" />
+                        <p>Pikachu</p>
                     </div>
+                    
+                </div>
+                
+                <div className={styles.pokemonsButtons}>
+                    <button><span className="material-symbols-outlined">first_page</span></button>
+                    <button><span className="material-symbols-outlined">chevron_left</span></button>
+                    <button><span className="material-symbols-outlined">chevron_right</span></button>
+                    <button><span className="material-symbols-outlined">last_page</span></button>
                 </div>
             </div>
         </div>
